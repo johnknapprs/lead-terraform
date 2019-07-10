0a597f3d515f0921858cdf278b5a72459fa685cc
Alex Nelson <alexsnelson95@gmail.com>
2019-07-10 16:13:34 -0700

    Setup azure provider and moved other providers to azure vars

--- stacks/environment-azure/main.tf
+++ stacks/environment-azure/main.tf
@@ -1,12 +1,12 @@
 terraform {
-  backend "s3" {}
+  backend "azurem" {}
 }
 
-provider "aws" {
+provider "azurem" {
   version = ">= 1.47.0"
-  region  = "${var.region}"
 }
 
+/*
 data "aws_eks_cluster" "cluster" {
   name = "${module.eks.cluster_id}"
 }
@@ -14,11 +14,12 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = "${data.aws_eks_cluster.cluster.name}"
 }
+*/
 
 provider "kubernetes" {
-  host = "${data.aws_eks_cluster.cluster.endpoint}"
-  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
-  token                  = "${data.aws_eks_cluster_auth.cluster.token}"
+  host = "${data.azure_aks_cluster.cluster.endpoint}"
+  cluster_ca_certificate = "${base64decode(data.azure_aks_cluster.cluster.certificate_authority.0.data)}"
+  token                  = "${data.azure_aks_cluster_auth.cluster.token}"
   load_config_file       = false
 }
 
@@ -29,9 +30,9 @@ provider "helm" {
   service_account = "${module.infrastructure.tiller_service_account}"
 
   kubernetes {
-    host                   = "${data.aws_eks_cluster.cluster.endpoint}"
-    cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
-    token                  = "${data.aws_eks_cluster_auth.cluster.token}"
+    host                   = "${data.azure_aks_cluster.cluster.endpoint}"
+    cluster_ca_certificate = "${base64decode(data.azure_aks_cluster.cluster.certificate_authority.0.data)}"
+    token                  = "${data.azure_aks_cluster_auth.cluster.token}"
     load_config_file       = false
   }
 }
@@ -43,9 +44,9 @@ provider "helm" {
   service_account = "${module.toolchain.tiller_service_account}"
 
   kubernetes {
-    host                   = "${data.aws_eks_cluster.cluster.endpoint}"
-    cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)}"
-    token                  = "${data.aws_eks_cluster_auth.cluster.token}"
+    host                   = "${data.azure_aks_cluster.cluster.endpoint}"
+    cluster_ca_certificate = "${base64decode(data.azure_aks_cluster.cluster.certificate_authority.0.data)}"
+    token                  = "${data.azure_aks_cluster_auth.cluster.token}"
     load_config_file       = false
   }
 }
